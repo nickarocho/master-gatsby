c5d4d95052a5ca757bcdc07e182b8e1b21624363
Nicholas.Arocho <nick@boldlyfine.com>
2020-09-22 23:47:41 -0700

    Add normalize.css and some global styles via Styled Components

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import 'normalize.css';
 
 export function wrapPageElement({ element, props }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyles from '../styles/GlobalStyles';
 import Footer from './Footer';
 import Nav from './Nav';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />

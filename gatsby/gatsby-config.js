377cb080d094b863d048e5300bff70d16ee2ee57
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-03 15:37:35 -0700

    create gatsby-config file to source the Sanity API within Gatsby

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,26 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Nick's Slices`,
+    siteUrl: `https://gatsby.pizza`,
+    description: `It's a PIZZA pie!`,
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'ejiqoazc',
+        dataset: 'production',
+        // when in development and you make a change to the CSS
+        // it will auto-update without needing to rebuild
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};

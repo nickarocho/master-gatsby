8cd27a480d025a38500474e198ecad98c75eb996
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-14 16:44:54 -0700

    Add a loading grid to the homepage with animation

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+    </div>
+  );
+}
+
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothin in the case!</p>}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
+
   return (
-    <>
-      <p>hey im the homepage</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }

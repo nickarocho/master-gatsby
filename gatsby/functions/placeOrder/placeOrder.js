c14e36703760041e60e611d62c7e7606950f071a
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-14 13:05:18 -0700

    Create backend validation and generate order email with dynamic data

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,32 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `
+    <div>
+      <h2>Your Recent Order for ${total}</h2>
+      <p>Please start walking over, we will have your order ready in the next 20 mins.</p>
+      <ul>
+        ${order
+          .map(
+            (item) => `
+              <li>
+                <img src="${item.thumbnail}" alt="${item.name}" />
+                ${item.size} ${item.name} - ${item.price}
+              </li>
+            `
+          )
+          .join('')}
+      </ul>
+      <p>Your total is <strong>$${total}</strong>, due at pickup</p>
+      <style>
+          ul {
+            list-style: none;
+          }
+      </style>
+    </div>
+  `;
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,18 +38,36 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+
+  // Validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // Send the email
+
+  // Send the success or error message
+
   // test send an email
   const info = await transporter.sendMail({
     from: "Nick's Slices <nick@example.com>",
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New Order!',
-    html: `
-      <p>Your new pizza order is here!</p>
-    `,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };

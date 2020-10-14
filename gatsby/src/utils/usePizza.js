599009e1fa9b2dbf3936c8037de56afa88dd40ef
Nicholas.Arocho <nick@boldlyfine.com>
2020-10-14 12:09:00 -0700

    Add client-side logic to handle and massage data to send to the server for an order

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,12 +1,18 @@
 import { useContext, useState } from 'react';
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizzas, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // 1. Create some state to hold the order
   // We got rid of this line because we moved useState up to the provider
   // const [order, setOrder] = useState([]);
   // now we access both our state and our updater fn via context
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   // 2. Make a function to add things to the order
   function addToOrder(orderedPizza) {
@@ -21,10 +27,52 @@ export default function usePizza({ pizzas, inputs }) {
       ...order.slice(index + 1),
     ]);
   }
-  // TODO: 4. Send this to a serverless fn when they check out
+
+  // this is the function that is run when someone submits the form
+  async function submitOrder(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    // gather all the data
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+    // 4. Send this to a serverless fn when they check out
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = await JSON.parse(res.text());
+
+    // check if everything worked
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false); // turn off loading...
+      setError(text.message);
+    } else {
+      // it worked!
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza');
+    }
+  }
+
+  // return the fn's from the hook to catch elsewhere
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
